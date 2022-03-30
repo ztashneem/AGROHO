a2e87f009ece3ac3de6da2013b293b607b2a066f
ztashneem <z.tashneem12@gmail.com>
2022-03-30 22:32:51 +0600

    responsive style added

---
+++ src/components/Products/Product.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+import './Product.css';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+const Product = (props) => {
+    // console.log(props.product);
+    const { name, img, price, Description, seller } = props.product;
+    return (
+        <div className='col-12 products ' style={{ height: '42rem' }}>
+            <>
+                <img src={img} alt="" className=' mb-4 img-fluid ' />
+                <h4>{name.slice(0, 30)}</h4>
+                <h4>Price:  $ {price}</h4>
+                <h6>Manufactured:  {seller}</h6>
+                <small className='text-justify'> <code>Description:</code> {Description.slice(0, 120)}...</small>
+
+                <span onClick={() => props.handdleAddRoCart(props.product)} className=' btn btn-outline-dark  button '> Add To Cart <MdAddShoppingCart className='ms-lg-2 ms-0 fw-bold'></MdAddShoppingCart> </span>
+            </>
+
+        </div>
+    );
+};
+
+export default Product;
