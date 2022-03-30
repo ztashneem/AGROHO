ba277eed6fbd535f3bc6c1501a2a6346f7e2bb5d
ztashneem <z.tashneem12@gmail.com>
2022-03-30 22:24:54 +0600

    shop and data json setup

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,9 +1,43 @@
-import React from 'react';
 
+import React, { useEffect, useState } from 'react';
+import Cart from '../cart/Cart';
+import Product from '../Products/Product';
+import './Shop.css'
 const Shop = () => {
+
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    //EVENT HANDDLEER
+    const handdleAddRoCart = (product) => {
+        // console.log('clicked', product);
+        const newCart = [...cart, product];
+        setCart(newCart)
+    }
     return (
-        <div>
-            <h1>Hello Agrohoer Ammu !!</h1>
+        <div className=' container-fulid  row mt-5'>
+            <div className="col-md-8 col-12 ms-lg-5">
+                <div className='product-container'>
+
+                    {
+                        products.map(product => <Product key={product.id} product={product} handdleAddRoCart={handdleAddRoCart} ></Product>)
+                    }
+
+                </div>
+
+            </div>
+            <div className="col-md-3 col-12 mx-auto">
+                <div className=''>
+
+                    <Cart cart={cart}></Cart>
+                </div>
+            </div>
         </div>
     );
 };
